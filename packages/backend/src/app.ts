4b8429788a9cb3ca67aa43c3da491cc9e72e6187
yihu55 <veronicayi@msn.com>
2022-10-04 21:14:17 +0200

    database config

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,20 +1,19 @@
 import express, { Application, json, Request, Response } from 'express'
 import cors from 'cors'
-import { config } from './config/db'
-import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { setUpMongoDb } from './models/common'
+
+dotenv.config()
 const app: Application = express()
+const port: number = parseInt(process.env.SERVER_PORT || '5000')
+const mongoUrl: string = process.env.MONGODB_URL || 'mongodb:127.0.0.1/chatapp'
 app.use(cors())
 app.use(json())
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Chat app📨')
 })
-app.listen(config.server.port, async function () {
-  await mongoose.connect(config.mongo.url).then(() => {
-    console.log('Database connected!')
-  })
-  // .catch(error){
-  //   console.log(error)
-  // }
-  console.log(`App is listening on port ${config.server.port} !`)
+app.listen(port, async function () {
+  await setUpMongoDb(mongoUrl)
+  console.log(`App is listening on port ${port} !`)
 })
