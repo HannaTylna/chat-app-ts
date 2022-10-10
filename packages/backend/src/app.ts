0eb2ddcfaf0901dd977ad397a1388b31e6c11b24
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-10 19:54:02 +0200

    change default variable for mongourl

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -6,7 +6,7 @@ import { setUpMongoDb } from './models/common'
 dotenv.config()
 const app: Application = express()
 const port: number = parseInt(process.env.SERVER_PORT || '5000')
-const mongoUrl: string = process.env.MONGODB_URL || 'mongodb:127.0.0.1/chatapp'
+const mongoUrl: string = process.env.MONGODB_URL || 'mongodb://localhost:27017/chatapp'
 app.use(cors())
 app.use(json())
 
