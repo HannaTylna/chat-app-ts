c810d8d561d5cc53fcd1082c0f8579b29ce772a6
MadSamson <m.mahdi.samadi@gmail.com>
2022-10-11 10:41:52 +0200

    store passwored in database w bcrypt

--- packages/backend/src/models/users.ts
+++ packages/backend/src/models/users.ts
@@ -1,9 +1,16 @@
 import { User } from '@chat-app/shared'
 import { model, Schema } from 'mongoose'
+import bcrypt from 'bcrypt'
 
 const userSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+})
+
+userSchema.pre<User>('save', async function (next) {
+  const hashedpassword = await bcrypt.hash(this.password, 10)
+  this.password = hashedpassword
+  next()
 })
 
 const UserModel = model<User>('User', userSchema)
