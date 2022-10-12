f0a5356729face5d511682447a092c3d890c9e75
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-10-12 22:17:38 +0200

    add error handling to controller

--- packages/backend/src/controller/user.controller.ts
+++ packages/backend/src/controller/user.controller.ts
@@ -1,6 +1,26 @@
+import { User } from '@chat-app/shared'
 import { Request, Response } from 'express'
-import { createNewUser } from '../services/user.services'
+import { UserModel } from '../models/users'
+import { registerUser } from '../services/user.services'
 
 export const addANewUser = async (req: Request, res: Response) => {
-  res.send(await createNewUser(req.body))
+  const body = req.body as User
+  try {
+    const existUser = await UserModel.findOne({ email: body.email })
+    if (existUser) {
+      res.send({ message: 'User is already exist!' })
+    }
+    if (body.username == '' || !body.username) {
+      res.send({ message: 'You need to enter username!' })
+    }
+    if (body.email == '' || !body.email) {
+      res.send({ message: 'You need to enter email!' })
+    }
+    if (body.password == '' || !body.password) {
+      res.send({ message: 'You need to enter password!' })
+    }
+    res.send(await registerUser(body))
+  } catch (error) {
+    return res.status(500)
+  }
 }
