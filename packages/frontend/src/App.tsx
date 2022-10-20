9d71264fd27e8f382af53f5f804182b3362a9b2a
yihu55 <veronicayi@msn.com>
2022-10-20 15:58:11 +0200

    signup works without docker

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import SignUpPage from './pages/SignUpPage'
 
 function App() {
   return (
     <div className='App'>
-      <h1>Chat app 📨</h1>
+      <Routes>
+        <Route path='/signup' element={<SignUpPage />} />
+      </Routes>
     </div>
   )
 }
---
+++ packages/frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+import ReactBubblyEffectButton from 'react-bubbly-effect-button'
+import axios from 'axios'
+import { Container, Row, Col, Alert, Button } from 'react-bootstrap'
+
+import { useNavigate } from 'react-router-dom'
+
+const SignUpPage = () => {
+  const navigate = useNavigate()
+
+  const [username, setUsername] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [message, setMessage] = useState<string>('')
+
+  const performSignup = async (): Promise<void> => {
+    console.log('sign up')
+    if (!username || !password || !email) {
+      setMessage('name, mail, password are required')
+      setTimeout(() => {
+        setMessage('')
+      }, 5000)
+    }
+    const signupResponse = await axios.post('http://localhost:4000/api/users', {
+      username: username,
+      email: email,
+      password: password,
+    })
+    console.log(signupResponse)
+    if (signupResponse.status === 201) {
+      console.log('user created')
+      setMessage('user created successfully')
+      setTimeout(() => {
+        navigate('/login')
+      }, 3000)
+    }
+  }
+  return (
+    <>
+      <h1>KIKI EXPRESS CHAT APP 📨</h1>
+      <Container>
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <label>Username</label>
+            <input
+              type='text'
+              value={username}
+              placeholder='Enter username'
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label>Mail</label>
+            <input
+              type='email'
+              value={email}
+              placeholder='Enter mail'
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <label>Password</label>
+            <input
+              type='password'
+              placeholder='Password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <div>
+              <ReactBubblyEffectButton text='SIGN UP' bgColor='#E23D28' onClick={performSignup} />
+            </div>
+
+            <p>
+              already user?
+              <Button variant='info' onClick={() => navigate('/login')}>
+                login
+              </Button>
+            </p>
+
+            {message && <Alert variant='danger'>{message}</Alert>}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
+}
+export default SignUpPage
