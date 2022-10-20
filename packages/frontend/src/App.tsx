b33d6233378007c0c8332f9a79471050ed44b34c
yihu55 <veronicayi@msn.com>
2022-10-20 19:18:55 +0200

    login page created

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import LoginPage from './pages/LoginPage'
 
 import SignUpPage from './pages/SignUpPage'
 
 function App() {
   return (
     <div className='App'>
-      <h1>frontend</h1>
       <Routes>
         <Route path='/signup' element={<SignUpPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={<LoginPage />} />
       </Routes>
     </div>
   )
