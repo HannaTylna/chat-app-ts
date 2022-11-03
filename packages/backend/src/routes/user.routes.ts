c607fb629a89d0c3855eccfbff31b032d5080457
MadSamson <m.mahdi.samadi@gmail.com>
2022-11-03 14:56:43 +0100

    add a route for userinfo

--- packages/backend/src/routes/user.routes.ts
+++ packages/backend/src/routes/user.routes.ts
@@ -1,8 +1,11 @@
 import express from 'express'
 import * as userController from '../controller/user.controller'
+import { authenticateToken } from '../middleware/auth'
 
 const route = express.Router()
 route.post('/', userController.addANewUser)
 route.post('/login', userController.loginUser)
+route.use('/userInfo', authenticateToken)
+route.get('/userInfo', userController.getUserInfo)
 
 export default route
